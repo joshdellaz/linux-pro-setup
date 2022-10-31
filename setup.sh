f9d335e80c05f00847a74fc60edf3e6940feb4f2
joshdellaz <josh.lazaruk@gmail.com>
2022-10-31 12:08:49 -0600

    add vscode and robot

--- setup.sh
+++ setup.sh
@@ -11,6 +11,16 @@ sudo apt-get install g++ -y
 sudo apt-get install doxygen -y
 sudo apt-get install git -y
 sudo apt-get install cmake -y
+pip install robotframework
+
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
 
 cd
 git clone https://github.com/gpakosz/.tmux.git
